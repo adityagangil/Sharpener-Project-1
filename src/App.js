2285c52ebe889008fe164df6253dfa5e4bcb9544
[aditya gangil] <[adityagangil2001@gmail.com]>
2023-11-22 18:57:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dve/api/films/').then(response => {
-      return response.json();
-    })
-    .then((data) => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
+  async function fetchMoviesHandler() {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }));
+
       setMovies(transformedMovies);
-    });
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler} disabled={isLoading}>
+          {isLoading ? 'Fetching...' : 'Fetch Movies'}
+        </button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
